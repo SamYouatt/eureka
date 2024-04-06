6bc6e4156f475350257227588ae68ac5e693d644
SamYouatt <sdyouatt@gmail.com>
2024-04-06 18:48:00 +0100

    read configuration in main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use eureka::startup::run;
+use eureka::{configuration::get_configuration, startup::run};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("0.0.0.0:42069").await.expect("Failed to bind listener");
-    let server = run(listener).await?;
+    let configuration = get_configuration().expect("Failed to read configuration");
 
+    let app_address = format!("0.0.0.0:{}", configuration.application_port);
+    let listener = TcpListener::bind(app_address).await.expect("Failed to bind listener");
+
+    let server = run(listener).await?;
     server.await
 }
