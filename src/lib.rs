8f7e25108be36f6083a5d12702babc9622cb42cd
SamYouatt <sdyouatt@gmail.com>
2024-04-06 11:47:48 +0100

    move everything else to lib.rs as well

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
 use std::sync::{Arc, Mutex};
 
 use axum::{routing::get, Router};
+use domain::idea::Idea;
+use features::{create_idea::handler::{create_idea, create_idea_page}, health_check::health_check, idea_list::handler::get_ideas, view_idea::handler::get_idea};
 use tower_http::services::ServeDir;
+
+mod domain;
+mod features;
+
+#[derive(Clone)]
+pub struct AppState {
+    ideas: Arc<Mutex<Vec<Idea>>>,
+}
 
 pub async fn run() {
     let ideas = Arc::new(Mutex::new(generate_seed_data()));
@@ -28,4 +38,48 @@ pub async fn run() {
         .unwrap();
 
     axum::serve(listener, app).await.unwrap();
+}
+
+fn generate_seed_data() -> Vec<Idea> {
+    let mut ideas = vec![];
+
+    ideas.push(Idea::new("This one", "Track your ideas"));
+    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
+    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
+    ideas.push(Idea::new(
+        "Bucket with a hole",
+        "Basically a sprikler but with a handle",
+    ));
+    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
+    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
+    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
+    ideas.push(Idea::new(
+        "Bucket with a hole",
+        "Basically a sprikler but with a handle",
+    ));
+    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
+    ideas.push(Idea::new(
+        "Bucket with a hole",
+        "Basically a sprikler but with a handle",
+    ));
+    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
+    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
+    ideas.push(Idea::new(
+        "Bucket with a hole",
+        "Basically a sprikler but with a handle",
+    ));
+    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
+    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
+    ideas.push(Idea::new(
+        "Bucket with a hole",
+        "Basically a sprikler but with a handle",
+    ));
+    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
+    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
+    ideas.push(Idea::new(
+        "Bucket with a hole",
+        "Basically a sprikler but with a handle",
+    ));
+
+    ideas
 }
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,7 @@
-use std::sync::{Arc, Mutex};
-
-use axum::{routing::get, Router};
-use domain::idea::Idea;
-use features::{
-    create_idea::handler::{create_idea, create_idea_page}, health_check::health_check, idea_list::handler::get_ideas, view_idea::handler::get_idea
-};
-use tower_http::services::ServeDir;
-
 use eureka::run;
-
-mod domain;
-mod features;
-
-#[derive(Clone)]
-pub struct AppState {
-    ideas: Arc<Mutex<Vec<Idea>>>,
-}
 
 #[tokio::main]
 async fn main() {
     run().await
 }
 
-fn generate_seed_data() -> Vec<Idea> {
-    let mut ideas = vec![];
-
-    ideas.push(Idea::new("This one", "Track your ideas"));
-    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
-    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
-    ideas.push(Idea::new(
-        "Bucket with a hole",
-        "Basically a sprikler but with a handle",
-    ));
-    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
-    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
-    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
-    ideas.push(Idea::new(
-        "Bucket with a hole",
-        "Basically a sprikler but with a handle",
-    ));
-    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
-    ideas.push(Idea::new(
-        "Bucket with a hole",
-        "Basically a sprikler but with a handle",
-    ));
-    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
-    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
-    ideas.push(Idea::new(
-        "Bucket with a hole",
-        "Basically a sprikler but with a handle",
-    ));
-    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
-    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
-    ideas.push(Idea::new(
-        "Bucket with a hole",
-        "Basically a sprikler but with a handle",
-    ));
-    ideas.push(Idea::new("Candle shop", "Electricity is overrated"));
-    ideas.push(Idea::new("Tartan paint", "Brighten up your day"));
-    ideas.push(Idea::new(
-        "Bucket with a hole",
-        "Basically a sprikler but with a handle",
-    ));
-
-    ideas
-}
