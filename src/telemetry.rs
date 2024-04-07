a5c7d19b07e64db48db59d9b31a7fa6d90dacd06
SamYouatt <sdyouatt@gmail.com>
2024-04-07 20:41:25 +0100

    forgot to use these params instead of hardcoded ones

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -5,8 +5,8 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 /// Compose multiple layers into a `tracing`'s Subscriber
 pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Sync + Send {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("eureka".into(), std::io::stdout);
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
 
     Registry::default()
         .with(env_filter)
@@ -17,7 +17,5 @@ pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Syn
 /// Register a subsriber as global default to process span data
 pub fn init_subscriber(subscriber: impl Subscriber + Sync + Send) {
     LogTracer::init().expect("Failed to set logger");
-
-
     set_global_default(subscriber).expect("Failed to set tracing subscriber");
 }
