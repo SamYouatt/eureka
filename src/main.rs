b6f683701724ff8e46f568e5ea8ab7929e88b3c4
SamYouatt <sdyouatt@gmail.com>
2024-04-07 20:38:00 +0100

    refactor: extract telemetry setup to library functions

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
-use eureka::{configuration::get_configuration, startup::run};
+use eureka::{configuration::get_configuration, startup::run, telemetry::{get_subscriber, init_subscriber}};
 use sqlx::PgPool;
 use tokio::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Tracing setup
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("eureka".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set tracing subscriber");
+    let subscriber = get_subscriber("eureka".into(), "info".into());
+    init_subscriber(subscriber);
 
     // Load configuration
     let configuration = get_configuration().expect("Failed to read configuration");
---
+++ src/telemetry.rs
@@ -0,0 +1,23 @@
+use tracing::{subscriber::set_global_default, Subscriber};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
+/// Compose multiple layers into a `tracing`'s Subscriber
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Sync + Send {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("eureka".into(), std::io::stdout);
+
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+/// Register a subsriber as global default to process span data
+pub fn init_subscriber(subscriber: impl Subscriber + Sync + Send) {
+    LogTracer::init().expect("Failed to set logger");
+
+
+    set_global_default(subscriber).expect("Failed to set tracing subscriber");
+}
