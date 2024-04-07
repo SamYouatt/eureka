fdccba7c8bf8f8e802d880801cbd317f24a4b959
SamYouatt <sdyouatt@gmail.com>
2024-04-07 14:42:43 +0100

    remove unneeded ideas in app state now we have db

--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,12 @@
-use std::sync::{Arc, Mutex};
-
 use axum::{routing::get, serve::Serve, Router};
 use sqlx::PgPool;
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
-use crate::{features::{create_idea::handler::{create_idea, create_idea_page}, health_check::health_check, idea_list::handler::get_ideas, view_idea::handler::get_idea}, generate_seed_data, AppState};
+use crate::{features::{create_idea::handler::{create_idea, create_idea_page}, health_check::health_check, idea_list::handler::get_ideas, view_idea::handler::get_idea}, AppState};
 
 pub async fn run(listener: TcpListener, db_pool: PgPool) -> Result<Serve<Router, Router>, std::io::Error> {
-    let ideas = Arc::new(Mutex::new(generate_seed_data()));
-
     let state = AppState {
-        ideas: ideas.clone(),
         db: db_pool,
     };
 
