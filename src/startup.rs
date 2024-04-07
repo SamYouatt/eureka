aea6469d07257f9ead6e6bb2bc772f4c2c58ed4d
SamYouatt <sdyouatt@gmail.com>
2024-04-07 19:43:16 +0100

    add tracing layer into app router

--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,33 @@
 use axum::{routing::get, serve::Serve, Router};
 use sqlx::PgPool;
 use tokio::net::TcpListener;
-use tower_http::services::ServeDir;
+use tower_http::{
+    services::ServeDir,
+    trace::{self, TraceLayer},
+};
+use tracing::Level;
 
-use crate::{features::{create_idea::handler::{create_idea, create_idea_page}, health_check::health_check, idea_list::handler::get_ideas, view_idea::handler::get_idea}, AppState};
+use crate::{
+    features::{
+        create_idea::handler::{create_idea, create_idea_page},
+        health_check::health_check,
+        idea_list::handler::get_ideas,
+        view_idea::handler::get_idea,
+    },
+    AppState,
+};
 
-pub async fn run(listener: TcpListener, db_pool: PgPool) -> Result<Serve<Router, Router>, std::io::Error> {
-    let state = AppState {
-        db: db_pool,
-    };
+pub async fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+) -> Result<Serve<Router, Router>, std::io::Error> {
+    let state = AppState { db: db_pool };
 
     let assets_path = std::env::current_dir().unwrap();
+
+    let trace_layer = TraceLayer::new_for_http()
+        .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+        .on_response(trace::DefaultOnResponse::new().level(Level::INFO));
 
     let app = Router::new()
         .route("/", get(get_ideas))
@@ -21,7 +38,8 @@ pub async fn run(listener: TcpListener, db_pool: PgPool) -> Result<Serve<Router,
         .nest_service(
             "/assets",
             ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
-        );
+        )
+        .layer(trace_layer);
 
     Ok(axum::serve(listener, app))
 }
