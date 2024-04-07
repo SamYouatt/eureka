aa16c9d979dde296f2e310640e63280b08aba954
SamYouatt <sdyouatt@gmail.com>
2024-04-07 20:37:45 +0100

    instrument health_check for completeness

--- src/features/health_check/mod.rs
+++ src/features/health_check/mod.rs
@@ -1,5 +1,7 @@
 use axum::{http::StatusCode, response::IntoResponse};
 
 pub async fn health_check() -> impl IntoResponse {
+    tracing::info!("Responding to health check, I'm alive!");
+
     StatusCode::OK
 }
