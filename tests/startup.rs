2ca5eed165415cf38e7e88266889ed9597b9d75a
SamYouatt <sdyouatt@gmail.com>
2024-04-07 12:25:06 +0100

    refactor: start new database for each test run means that the state will be wiped clean each run, much better for repeatable test runs. Alternative would be wrapping every query in a transaction and rolling it back

--- tests/startup.rs
+++ tests/startup.rs
@@ -1,6 +1,10 @@
-use eureka::{configuration::get_configuration, startup::run};
-use sqlx::PgPool;
+use eureka::{
+    configuration::{get_configuration, DatabaseSettings},
+    startup::run,
+};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use tokio::net::TcpListener;
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -8,16 +12,19 @@ pub struct TestApp {
 }
 
 pub async fn spawn_test_app() -> TestApp {
-    let listener = TcpListener::bind("0.0.0.0:0").await.expect("Failed to bind listener");
+    let listener = TcpListener::bind("0.0.0.0:0")
+        .await
+        .expect("Failed to bind listener");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let db_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let mut configuration = get_configuration().expect("Failed to read configuration");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let db_pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, db_pool.clone()).await.expect("Failed to spawn server");
+    let server = run(listener, db_pool.clone())
+        .await
+        .expect("Failed to spawn server");
 
     let _ = tokio::spawn(async { server.await });
 
@@ -25,4 +32,27 @@ pub async fn spawn_test_app() -> TestApp {
         address,
         db: db_pool,
     }
+}
+
+// Create a new database instance for each test
+async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database");
+
+    let db_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    
+    sqlx::migrate!("./migrations")
+        .run(&db_pool)
+        .await
+        .expect("Failed to run database migrations");
+
+    db_pool
 }
