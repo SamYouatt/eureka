e95f16748977719626e5f8339c82cacac86f01dd
SamYouatt <sdyouatt@gmail.com>
2024-04-08 09:14:13 +0100

    instrument: new idea route

--- src/features/create_idea/handler.rs
+++ src/features/create_idea/handler.rs
@@ -6,13 +6,14 @@ use axum::{
 };
 use chrono::Utc;
 use serde::Deserialize;
-use sqlx::query;
+use sqlx::{query, PgPool};
 use uuid::Uuid;
 
 use crate::{domain::page::page, AppState};
 
 use super::views::new_idea_form;
 
+#[tracing::instrument(name = "Render new idea form")]
 pub async fn create_idea_page() -> impl IntoResponse {
     page(new_idea_form())
 }
@@ -23,29 +24,46 @@ pub struct NewIdea {
     tagline: String,
 }
 
+#[tracing::instrument(
+    name="Creating new idea",
+    skip(state, new_idea),
+    fields(
+        request_id = %Uuid::new_v4(),
+        idea_title = %new_idea.name,
+    )
+)]
 pub async fn create_idea(
     State(state): State<AppState>,
     Form(new_idea): Form<NewIdea>,
 ) -> impl IntoResponse {
-    tracing::info!("Inserting new idea into database: {:?}", new_idea);
-    if let Err(e) = query!(
+    match insert_idea(&state.db, new_idea).await {
+        Ok(_) => {
+            let mut headers = HeaderMap::new();
+            headers.insert("HX-Redirect", "/".parse().unwrap());
+
+            (headers, StatusCode::OK)
+        }
+        Err(_) => {
+            (HeaderMap::new(), StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
+}
+
+#[tracing::instrument(name = "Saving new idea to database", skip(db, idea))]
+async fn insert_idea(db: &PgPool, idea: NewIdea) -> Result<(), sqlx::Error> {
+    query!(
         "INSERT INTO ideas (id, title, tagline, created_at) VALUES ($1, $2, $3, $4)",
         Uuid::new_v4(),
-        new_idea.name,
-        new_idea.tagline,
+        idea.name,
+        idea.tagline,
         Utc::now()
     )
-    .execute(&state.db)
+    .execute(db)
     .await
-    {
-        tracing::error!("Failed to execute query: {:?}", e);
-        return (HeaderMap::new(), StatusCode::INTERNAL_SERVER_ERROR);
-    }
+    .map_err(|e| {
+        tracing::error! {"Failed to execute query: {:?}", e};
+        e
+    })?;
 
-    tracing::info!("Idea inserted succesfully");
-
-    let mut headers = HeaderMap::new();
-    headers.insert("HX-Redirect", "/".parse().unwrap());
-
-    (headers, StatusCode::OK)
+    Ok(())
 }
