dc95f5c2b26d3e1649fb319f31264d8ab3c03638
SamYouatt <sdyouatt@gmail.com>
2024-04-08 10:03:21 +0100

    instrument: get idea route

--- src/features/view_idea/handler.rs
+++ src/features/view_idea/handler.rs
@@ -2,7 +2,7 @@ use axum::{
     extract::{Path, State},
     response::IntoResponse,
 };
-use sqlx::query_as;
+use sqlx::{query_as, PgPool};
 use uuid::Uuid;
 
 use super::views::{idea_view, missing_idea};
@@ -13,27 +13,29 @@ pub struct Idea {
     pub tagline: String,
 }
 
+#[tracing::instrument(name = "Get idea", skip(state))]
 pub async fn get_idea(State(state): State<AppState>, Path(id): Path<String>) -> impl IntoResponse {
     let id = match Uuid::parse_str(&id) {
         Ok(id) => id,
         Err(e) => {
             tracing::error!("Failed to parse uuid from {:?}: {:?}", id, e);
-            return missing_idea()
-        },
-    };
-
-    tracing::info!("Loading idea {} from db", id);
-    let query = query_as!(Idea, "SELECT title, tagline FROM ideas WHERE id = $1", id);
-
-    let idea = match query.fetch_one(&state.db).await {
-        Ok(idea) => idea,
-        Err(e) => {
-            println!("Failed to find idea {}: {}", id, e);
             return missing_idea();
-        },
+        }
     };
 
-    tracing::info!("Fetched idea {} successfully", id);
+    match fetch_idea(id, &state.db).await {
+        Ok(idea) => idea_view(&idea),
+        Err(_) => missing_idea(),
+    }
+}
 
-    idea_view(&idea)
+#[tracing::instrument(name = "Fetch idea from database", skip(db))]
+async fn fetch_idea(id: Uuid, db: &PgPool) -> Result<Idea, sqlx::Error> {
+    query_as!(Idea, "SELECT title, tagline FROM ideas WHERE id = $1", id)
+        .fetch_one(db)
+        .await
+        .map_err(|e| {
+            tracing::error!("Failed to execute query: {:?}", e);
+            e
+        })
 }
