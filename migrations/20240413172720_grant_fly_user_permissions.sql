0fe97f9e642299c1986a87fe1ad8e91ac7c45b9c
SamYouatt <sdyouatt@gmail.com>
2024-04-13 19:21:30 +0100

    modify migration to only apply grant if user exists the tests don't have this user so ignore if for those

--- migrations/20240413172720_grant_fly_user_permissions.sql
+++ migrations/20240413172720_grant_fly_user_permissions.sql
@@ -1,1 +1,10 @@
-GRANT ALL PRIVILEGES ON DATABASE eureka TO sam_y_eureka;
+DO $$
+DECLARE
+    found_user pg_catalog.pg_roles%rowtype;
+BEGIN
+    SELECT * INTO found_user FROM pg_catalog.pg_roles WHERE rolname='sam_y_eureka';
+
+    IF FOUND THEN
+        GRANT ALL PRIVILEGES ON DATABASE eureka TO sam_y_eureka;
+    END IF;
+END $$;
