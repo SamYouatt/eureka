d6824b920add4f608d55aa52cac103142027ebe4
SamYouatt <sdyouatt@gmail.com>
2024-04-20 15:11:26 +0100

    set up route to handle redirect from google

--- src/features/auth/handler.rs
+++ src/features/auth/handler.rs
@@ -1,8 +1,17 @@
-use axum::{response::IntoResponse, Extension};
+use axum::{extract::Query, http::StatusCode, response::IntoResponse, Extension};
 use oauth2::basic::BasicClient;
 use serde::Deserialize;
 
 use super::views::login_button;
+
+#[derive(Deserialize, Debug)]
+pub struct AuthRequest {
+    code: String,
+}
+
+pub async fn handle_login_redirect(Query(auth_request): Query<AuthRequest>, Extension(oauth_client): Extension<BasicClient>) -> impl IntoResponse {
+    todo!()
+}
 
 pub async fn login(Extension(oauth_client): Extension<BasicClient>) -> impl IntoResponse {
     login_button(
