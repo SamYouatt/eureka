f36aee6cf207e29a1f58b2bd87292858b6af7ba9
SamYouatt <sdyouatt@gmail.com>
2024-04-22 20:17:56 +0100

    Red: user should not be able to view another user's idea

--- tests/features/view_idea.rs
+++ tests/features/view_idea.rs
@@ -9,8 +9,8 @@ async fn can_view_idea() {
     // Arrange
     let test_app = spawn_test_app().await;
 
-    create_user_session(&test_app).await;
-    let idea_id = seed_idea(&test_app.db, "Test idea", "Just for testing").await;
+    let user_id = create_user_session("test@test.com", &test_app).await;
+    let idea_id = seed_idea(&test_app.db, "Test idea", "Just for testing", user_id).await;
 
     // Act
     let url = format!("{}/ideas/{}", test_app.address, idea_id);
@@ -29,15 +29,51 @@ async fn can_view_idea() {
     assert_eq!(1, response_body.match_indices("Just for testing").count());
 }
 
-async fn seed_idea(db: &PgPool, title: &str, tagline: &str) -> Uuid {
+#[tokio::test]
+async fn cannot_view_another_user_idea() {
+    // Arrange
+    let test_app = spawn_test_app().await;
+    let alice_user = Uuid::new_v4();
+    seed_user(alice_user, "alice@test.com", &test_app.db).await;
+    let idea_id = seed_idea(&test_app.db, "Alice's idea", "Just for testing", alice_user).await;
+
+    let _bob_user = create_user_session("bob@test.com", &test_app).await;
+
+    // Act
+    let url = format!("{}/ideas/{}", test_app.address, idea_id);
+    let response = test_app
+        .client
+        .get(url)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 401);
+}
+
+async fn seed_user(user_id: Uuid, email: &str, db: &PgPool) {
+    sqlx::query!(
+        "INSERT INTO users (id, email, created_at) VALUES ($1, $2, $3)",
+        user_id,
+        email,
+        Utc::now(),
+    )
+    .execute(db)
+    .await
+    .unwrap();
+}
+
+async fn seed_idea(db: &PgPool, title: &str, tagline: &str, user_id: Uuid) -> Uuid {
     let idea_id = Uuid::new_v4();
 
     sqlx::query!(
-        "INSERT INTO ideas (id, title, tagline, created_at) VALUES ($1, $2, $3, $4)",
+        "INSERT INTO ideas (id, title, tagline, created_at, user_id) VALUES ($1, $2, $3, $4, $5)",
         idea_id,
         title.into(),
         tagline.into(),
-        Utc::now()
+        Utc::now(),
+        user_id
     )
     .execute(db)
     .await
