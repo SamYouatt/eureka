190c423ea625c1eaf71c2abbd96886650d23b3aa
SamYouatt <sdyouatt@gmail.com>
2024-04-22 09:32:23 +0100

    refactor: this stuff lives in the helpers now

--- tests/features/auth.rs
+++ tests/features/auth.rs
@@ -1,58 +1,9 @@
 use sqlx::types::Uuid;
 use chrono::Utc;
 use sqlx::PgPool;
-use wiremock::{
-    matchers::{method, path},
-    Mock, MockGuard, ResponseTemplate,
-};
 
-use crate::helpers::{spawn_test_app, assert_redirect_to, TestApp};
+use crate::helpers::{assert_redirect_to, configure_open_id_mock, spawn_test_app};
 
-#[derive(serde::Serialize)]
-struct UseInfoResponse {
-    email: String,
-}
-
-#[derive(serde::Serialize)]
-struct AuthTokenResponse {
-    access_token: String,
-    token_type: String,
-    expires_in: u64,
-}
-
-impl AuthTokenResponse {
-    fn new() -> Self {
-        Self {
-            access_token: "test_access_token".into(),
-            token_type: "basic".into(),
-            expires_in: 3600,
-        }
-    }
-}
-
-async fn configure_open_id_mock(test_app: &TestApp) -> (MockGuard, MockGuard) {
-    // These routes are mocked and not the real routes used by Google
-    let auth_token_response = ResponseTemplate::new(200).set_body_json(AuthTokenResponse::new());
-    let mock_token_exchange = Mock::given(path("/token"))
-        .and(method("POST"))
-        .respond_with(auth_token_response)
-        .named("OpenId auth token exchange")
-        .mount_as_scoped(&test_app.open_id_client)
-        .await;
-
-    let user_info_response = ResponseTemplate::new(200).set_body_json(UseInfoResponse {
-        email: "test@test.com".into(),
-    });
-    let mock_user_info = Mock::given(path("/user"))
-        .and(method("GET"))
-        .respond_with(user_info_response)
-        .named("OpenId user info")
-        .expect(1)
-        .mount_as_scoped(&test_app.open_id_client)
-        .await;
-
-    (mock_token_exchange, mock_user_info)
-}
 
 #[tokio::test]
 async fn oauth_callback_attaches_cookie() {
