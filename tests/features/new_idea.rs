5b0b1477d9726ebe892dbc5654ea05f75ca4a337
SamYouatt <sdyouatt@gmail.com>
2024-04-22 20:00:36 +0100

    Red: idea should be created for that user

--- tests/features/new_idea.rs
+++ tests/features/new_idea.rs
@@ -1,10 +1,10 @@
 use crate::helpers::{create_user_session, spawn_test_app};
 
 #[tokio::test]
-async fn can_creat_new_idea() {
+async fn can_create_new_idea() {
     // Arrange
     let test_app = spawn_test_app().await;
-    create_user_session(&test_app).await;
+    let user_id = create_user_session("test@test.com", &test_app).await;
 
     let body = "name=Test%20Idea&tagline=Just%20Testing";
 
@@ -22,7 +22,7 @@ async fn can_creat_new_idea() {
     // Assert
     assert!(response.status().is_success());
 
-    let created_idea = sqlx::query!("SELECT title, tagline FROM ideas")
+    let created_idea = sqlx::query!("SELECT title, tagline FROM ideas WHERE user_id = $1", user_id)
         .fetch_one(&test_app.db)
         .await
         .expect("Failed to fetch saved idea");
