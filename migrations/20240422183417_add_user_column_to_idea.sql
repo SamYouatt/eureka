40c2f1b7179f35ce882da85d731ee7ddc746cc64
SamYouatt <sdyouatt@gmail.com>
2024-04-22 20:49:21 +0100

    migrations: I can just remove the unique now actually

--- migrations/20240422183417_add_user_column_to_idea.sql
+++ migrations/20240422183417_add_user_column_to_idea.sql
@@ -1,2 +1,2 @@
 -- Add user column to ideas table
-ALTER TABLE ideas ADD COLUMN user_id UUID NOT NULL UNIQUE REFERENCES users (id);
+ALTER TABLE ideas ADD COLUMN user_id UUID NOT NULL REFERENCES users (id);
--- migrations/20240422193744_remove_unique_constraint_on_user_id_in_ideas_table.sql
+++
@@ -1,2 +0,0 @@
--- Remove user id unique constraint on ideas
-ALTER TABLE ideas DROP CONSTRAINT ideas_user_id_key;
