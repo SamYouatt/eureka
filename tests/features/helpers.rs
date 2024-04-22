50c9e7e9a08c358c2d0a4222e1b4787c2a06d747
SamYouatt <sdyouatt@gmail.com>
2024-04-22 09:31:18 +0100

    set up helper for creating logged in user with cookie

--- tests/features/helpers.rs
+++ tests/features/helpers.rs
@@ -6,7 +6,7 @@ use eureka::{
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
-use wiremock::MockServer;
+use wiremock::{matchers::{method, path}, Mock, MockGuard, MockServer, ResponseTemplate};
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let subscriber_name = "test".to_string();
@@ -49,7 +49,7 @@ pub async fn spawn_test_app() -> TestApp {
     let application = Application::build(configuration.clone())
         .await
         .expect("Failed to build appliaction");
-    let address = format!("http:127.0.0.1:{}", application.port());
+    let address = format!("http://{}:{}", configuration.application.domain, application.port());
     let _ = tokio::spawn(application.run_until_stopped());
 
     TestApp {
@@ -80,6 +80,64 @@ async fn configure_and_migrate_db(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to run database migrations");
 
     db_pool
+}
+
+#[derive(serde::Serialize)]
+struct UseInfoResponse {
+    email: String,
+}
+
+#[derive(serde::Serialize)]
+struct AuthTokenResponse {
+    access_token: String,
+    token_type: String,
+    expires_in: u64,
+}
+
+impl AuthTokenResponse {
+    fn new() -> Self {
+        Self {
+            access_token: "test_access_token".into(),
+            token_type: "basic".into(),
+            expires_in: 3600,
+        }
+    }
+}
+
+pub async fn configure_open_id_mock(test_app: &TestApp) -> (MockGuard, MockGuard) {
+    // These routes are mocked and not the real routes used by Google
+    let auth_token_response = ResponseTemplate::new(200).set_body_json(AuthTokenResponse::new());
+    let mock_token_exchange = Mock::given(path("/token"))
+        .and(method("POST"))
+        .respond_with(auth_token_response)
+        .named("OpenId auth token exchange")
+        .mount_as_scoped(&test_app.open_id_client)
+        .await;
+
+    let user_info_response = ResponseTemplate::new(200).set_body_json(UseInfoResponse {
+        email: "test@test.com".into(),
+    });
+    let mock_user_info = Mock::given(path("/user"))
+        .and(method("GET"))
+        .respond_with(user_info_response)
+        .named("OpenId user info")
+        .expect(1)
+        .mount_as_scoped(&test_app.open_id_client)
+        .await;
+
+    (mock_token_exchange, mock_user_info)
+}
+
+// Creates a loggen in user with session cookie
+pub async fn run_login(client: &reqwest::Client, test_app: &TestApp) {
+    let _mock_open_id = configure_open_id_mock(test_app).await;
+
+    let url = format!("{}/login/redirect?code=testauthcode", test_app.address);
+    let _response = &client
+        .get(url)
+        .send()
+        .await
+        .expect("Failed to execute request");
 }
 
 pub fn assert_redirect_to(response: &reqwest::Response, location: &str) {
