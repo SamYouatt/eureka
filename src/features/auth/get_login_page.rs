71b8f1ac8f97ca1b863216cac8fa63541a6d03d1
SamYouatt <sdyouatt@gmail.com>
2024-04-23 21:42:46 +0100

    polish: nice ui for the login button

--- src/features/auth/get_login_page.rs
+++ src/features/auth/get_login_page.rs
@@ -1,10 +1,12 @@
 use axum::{response::IntoResponse, Extension};
 use maud::{html, Markup};
 
-use crate::configuration::OpenIdClient;
+use crate::{configuration::OpenIdClient, domain::page::page};
 
-pub async fn handle_get_login(Extension(oauth_client): Extension<OpenIdClient>) -> impl IntoResponse {
-    login_button(&oauth_client)
+pub async fn handle_get_login(
+    Extension(oauth_client): Extension<OpenIdClient>,
+) -> impl IntoResponse {
+    page(login_button(&oauth_client))
 }
 
 fn login_button(open_id_client: &OpenIdClient) -> Markup {
@@ -20,6 +22,17 @@ fn login_button(open_id_client: &OpenIdClient) -> Markup {
     );
 
     html! {
-        a href=(url) { "Log in with Google" }
+        div class="flex w-full h-full justify-center items-center" {
+            div class="flex flex-col px-6 py-4 gap-4 bg-white rounded-md border-2 border-slate-200 dark:bg-slate-800 dark:border-slate-600" {
+                p class="text-xl font-medium text-slate-700 dark:text-slate-100" { "Get started with Eureka..." }
+                hr class="border-dashed border-slate-200 dark:border-slate-600" ;
+                a href=(url)
+                    class="flex justify-center items-center px-4 py-2 gap-2 border border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:bg-slate-200 dark:hover:bg-slate-700 transition-colors duration-100"
+                    {
+                        img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" alt="google logo";
+                        span { "Log in with Google " }
+                    }
+            }
+        }
     }
 }
