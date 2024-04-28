5c77b33a28a43460ba456f91304c5bbc488ca894
SamYouatt <sdyouatt@gmail.com>
2024-04-28 13:53:40 +0100

    test skeleton

--- tests/features/main.rs
+++ tests/features/main.rs
@@ -4,3 +4,4 @@ mod helpers;
 mod new_idea;
 mod view_idea;
 mod view_idea_list;
+mod stories;
---
+++ tests/features/stories.rs
@@ -0,0 +1,9 @@
+#[tokio::test]
+async fn can_create_story() {
+    todo!()
+}
+
+#[tokio::test]
+async fn can_view_stories() {
+    todo!() 
+}
